b340d2741cf90731843f72e0770e38cb94105e83
Mstars_lab1_16 <mstarsacademy@MStarss-Anar.local>
2021-12-07 12:31:24 +0800

    Components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,5 +1,4 @@
 const title = 'My first react element by babel';
-
 const description = 'Babel P element';
 
 const header = (
@@ -9,8 +8,17 @@ const header = (
     </header>
 )
 
+function Header() {
+    return(
+        <header>
+            <h1>Score Board</h1>
+            <span className="stats">Player: 1</span>
+        </header>
+    )
+}
+
 ReactDOM.render(
-    header,
+    <Header/>,
     document.getElementById('root')
 )
 
